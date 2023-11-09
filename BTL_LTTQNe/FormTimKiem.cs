90f787e0d9a8cea1ce09c1edf1f26f13bd7b3332
Gunbon223 <88621181+Gunbon223@users.noreply.github.com>
2023-11-09 22:20:50 +0700

    timkiem1

--- BTL_LTTQNe/FormTimKiem.cs
+++ BTL_LTTQNe/FormTimKiem.cs
@@ -8,6 +8,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+//-Form Tìm Kiếm:
+
+//Tìm kiếm nguyên liệu theo tên, công dụng và chống chỉ định 
+//Tìm kiếm món ăn theo loại, công dụng và tên món ăn
+
 namespace BTL_LTTQNe
 {
     public partial class FormTimKiem : Form
