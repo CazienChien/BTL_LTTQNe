ca2a173da96da90e2c1c1b1887096a813690f79f
Gunbon223 <gunbon610@gmail.com>
2023-11-18 18:27:21 +0700

    updatêmnu

--- BTL_LTTQNe/Form1.Designer.cs
+++ BTL_LTTQNe/Form1.Designer.cs
@@ -28,37 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnTKNL = new System.Windows.Forms.Button();
+            this.btnTKMA = new System.Windows.Forms.Button();
+            this.btnQLMA = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnTKNL
             // 
-            this.button1.Location = new System.Drawing.Point(557, 307);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnTKNL.Location = new System.Drawing.Point(358, 111);
+            this.btnTKNL.Name = "btnTKNL";
+            this.btnTKNL.Size = new System.Drawing.Size(117, 32);
+            this.btnTKNL.TabIndex = 0;
+            this.btnTKNL.Text = "Tìm kiếm nguyên liệu";
+            this.btnTKNL.UseVisualStyleBackColor = true;
+            this.btnTKNL.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // btnTKMA
             // 
-            this.button2.Location = new System.Drawing.Point(646, 307);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.btnTKMA.Location = new System.Drawing.Point(550, 111);
+            this.btnTKMA.Name = "btnTKMA";
+            this.btnTKMA.Size = new System.Drawing.Size(120, 32);
+            this.btnTKMA.TabIndex = 1;
+            this.btnTKMA.Text = "Tìm kiếm món ăn";
+            this.btnTKMA.UseVisualStyleBackColor = true;
+            this.btnTKMA.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnQLMA
+            // 
+            this.btnQLMA.Location = new System.Drawing.Point(158, 111);
+            this.btnQLMA.Name = "btnQLMA";
+            this.btnQLMA.Size = new System.Drawing.Size(117, 32);
+            this.btnQLMA.TabIndex = 2;
+            this.btnQLMA.Text = "Thêm món ăn";
+            this.btnQLMA.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(733, 366);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(998, 515);
+            this.Controls.Add(this.btnQLMA);
+            this.Controls.Add(this.btnTKMA);
+            this.Controls.Add(this.btnTKNL);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -68,8 +79,9 @@
 
 		#endregion
 
-		private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button btnTKNL;
+        private System.Windows.Forms.Button btnTKMA;
+        private System.Windows.Forms.Button btnQLMA;
     }
 }
 
